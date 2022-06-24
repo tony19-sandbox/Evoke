1b50c2306077dd25e9a2c98fbd0d26f4be14e2f6
Tony Trinh <tony19@gmail.com>
2022-06-23 23:36:17 -0500

    chore: update dist

--- Evoke-Web/Architecture/Application Layer/client/dist/js/about.1f2730fb.js
+++ Evoke-Web/Architecture/Application Layer/client/dist/js/about.1f2730fb.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkevoke_web"]=self["webpackChunkevoke_web"]||[]).push([[443],{4335:function(e,n,u){u.r(n),u.d(n,{default:function(){return f}});var t=u(3396);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function c(e,n){return(0,t.wg)(),(0,t.iD)("div",a,s)}var o=u(89);const i={},b=(0,o.Z)(i,[["render",c]]);var f=b}}]);
-//# sourceMappingURL=about.c01ce870.js.map
+//# sourceMappingURL=about.1f2730fb.js.map
